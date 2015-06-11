1e962f0c4a5ef673bc94c04da49f21db849d4db5
Miguel Angel Pineda <mpinedacruz@gmail.com>
2015-06-11 18:52:34 +0200

    Crear Pregunta Quiz

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -39,3 +39,26 @@ exports.answer = function(req,res){
     }
     res.render('quizes/answer',{quiz: req.quiz, respuesta:resultado});
 };
+
+//GET /quizes/new
+exports.new = function(req,res){
+   //crea un objeto quiz
+    var quiz = models.Quiz.build(
+       {pregunta: "Pregunta", respuesta: "Respuesta"}
+   );
+
+    res.render('quizes/new',{quiz: quiz});
+};
+
+//GET /quizes/create
+exports.create = function(req,res){
+
+    var quiz = models.Quiz.build(req.body.quiz);
+
+    //Guarda en DB
+    quiz.save({fields:["pregunta", "respuesta"]}).then(
+        function(){
+            res.redirect('/quizes');
+        }
+    );
+};
--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,10 @@ router.param('quizId',quizController.load);  // autoload :quizId
 
 /* GET /quizes */
 router.get('/quizes', quizController.index);
-//router.get('/quizes?search')
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create',quizController.create);
 
 
 /* GET author page. */
