6b28eb3885079833a8c5d4b221cd33e5338db2ec
Miguel Angel Pineda <mpinedacruz@gmail.com>
2015-06-13 12:11:41 +0200

    moderación de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -4,6 +4,22 @@
 var models = require('../models/models.js');
 
 
+//autoload :id de comentarios
+exports.load = function(req, res, next, commentId){
+  models.Comment.find({
+    where: {
+        id: Number(commentId)
+    }
+    }).then(function(comment){
+        if(comment){
+            req.comment = comment;
+            next();
+        }else{next(new Error('No existe commentId=' + commentId));}
+    }).catch(function(error){next(error);});
+};
+
+
+
 //GET /quizes/:quidId/comments/new
 exports.new = function(req,res){
     res.render('comments/new',{quizid: req.params.quizId, errors:[]});
@@ -30,5 +46,13 @@ exports.create = function(req,res){
                 );
             }
     }).catch(function(error){next(error)})
+};
 
+//GET /quizes/:quidId/comments/:commentId/publish
+exports.publish = function(req,res){
+
+    req.comment.publicado = true;
+    req.comment.save({fields: ["publicado"]})
+        .then(function(){res.redirect('/quizes/'+ req.params.quizId);})
+        .catch(function(error){next(error);});
 };
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId',quizController.load);  // autoload :quizId
+router.param('commentId',commentController.load); //autoload :commentId
 
 //Definición de rutas de sesion
 router.get('/login', sessionController.new);
@@ -31,7 +32,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 /* GET author page. */
 router.get('/author', function(req, res) {
